7c906f5191f84843dcfc516ad0e4e54e13805d7c
Ishan Taparia <dualstrike99@gmail.com>
2015-04-21 15:53:46 -0500

    Added bitmap and custom font

--- src/main.c
+++ src/main.c
@@ -2,8 +2,12 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
-static void update_time() {
+static void update_time() 
+{
   // Get a tm structure
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
@@ -12,29 +16,46 @@ static void update_time() {
   static char buffer[] = "00:00";
 
   // Write the current hours and minutes into the buffer
-  if(clock_is_24h_style() == true) {
-    // Use 24 hour format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
-  } else {
+  if(clock_is_24h_style() == true) 
+  {
+    
     // Use 12 hour format
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    
+  } 
+  else 
+  {
+    
+    // Use 24 hour format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    
   }
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
+  
 }
 
 static void main_window_load(Window *window) 
 {
-
+  
+  // Create GBitmap, then set to created BitmapLayer
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 0, 144, 50));
+  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorCobaltBlue);
 
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+  
   // Improve the layout to be more like a watchface
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentRight);
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
@@ -46,6 +67,9 @@ static void main_window_unload(Window *window)
 
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+  
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
   
 }
 
