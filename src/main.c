070ecaf6b23bacd1d8c7980974ce7eacbfdf4e8b
Ishan Taparia <dualstrike99@gmail.com>
2015-04-22 23:44:00 -0500

    Weather elements setup

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
 #include <pebble.h>
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
+static GFont s_time_font;
+static GFont s_weather_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
+static TextLayer *s_time_layer;
+static TextLayer *s_weather_layer;
 
 static void update_time() 
 {
@@ -49,9 +56,20 @@ static void main_window_load(Window *window)
   s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorCobaltBlue);
+  // Create temperature Layer
+  s_weather_layer = text_layer_create(GRect(0, 130, 144, 25));
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_text_color(s_weather_layer, GColorWhite);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_layer, "Loading...");
 
   // Create GFont
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+  
+  // apply it and add to Window
+  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
+  text_layer_set_font(s_weather_layer, s_weather_font);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
   
   // Improve the layout to be more like a watchface
   text_layer_set_font(s_time_layer, s_time_font);
@@ -71,6 +89,10 @@ static void main_window_unload(Window *window)
   // Unload GFont
   fonts_unload_custom_font(s_time_font);
   
+  // Destroy weather elements
+  text_layer_destroy(s_weather_layer);
+  fonts_unload_custom_font(s_weather_font);
+  
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) 
@@ -79,6 +101,28 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
   update_time();
   
 }
+
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) 
+{
+
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) 
+{
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) 
+{
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) 
+{
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
 
 static void init() 
 {
@@ -100,6 +144,16 @@ static void init()
   
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
   
 }
 
