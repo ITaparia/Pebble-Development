f351926aeede99050143bb0c8c5b95ac802fcd22
Ishan Taparia <dualstrike99@gmail.com>
2015-12-02 14:24:33 -0600

    TimeTick Service Functionality

--- src/main.c
+++ src/main.c
@@ -3,6 +3,20 @@ static Window *s_main_window;
 
 static TextLayer *s_time_layer;
 
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
 static void main_window_load(Window *window) {
 
   // Get information about the Window
@@ -16,7 +30,6 @@ static void main_window_load(Window *window) {
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
@@ -26,7 +39,12 @@ static void main_window_load(Window *window) {
 }
 
 static void main_window_unload(Window *window) {
+   // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+}
 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
 static void init() {
@@ -43,11 +61,20 @@ static void init() {
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
   
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  update_time();
+  
 }
 
 static void deinit() {
-// Destroy Window
+  
+  // Destroy Window
   window_destroy(s_main_window);
+  
+ 
+  
 }
 
 int main(void) {
